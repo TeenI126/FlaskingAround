839df5724ea3605f2ce65f9bd8a2f48c4f935ae3
ismai143 <mateen.ismail@mail.utoronto.ca>
2024-08-12 21:41:15 -0400

    learning about variable rules

--- simon.py
+++ simon.py
@@ -1,6 +1,7 @@
 # let's go through the quickstart doc for Flask
 
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -8,3 +9,7 @@ app = Flask(__name__)
 @app.route("/")
 def my_cat():
     return "<p>My cat is called Simon!</p>"
+
+@app.route("/escape/<name>")
+def using_escape(name):
+    return f"my cat is named {escape(name)}"
